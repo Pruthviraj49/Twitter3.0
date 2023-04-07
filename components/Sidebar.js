c55028fa11501ebda4ab8d1e1096e49e4bc9759c
Pruthviraj49 <pdsuryawanshi45@gmail.com>
2023-04-07 12:42:01 +0530

    Initial Commit

---
+++ components/Sidebar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {FiMoreHorizontal} from 'react-icons/fi'
+import { VscTwitter } from 'react-icons/vsc'
+
+
+const style = {
+  wrapper: `flex-[0.7] px-8 flex flex-col`,
+  twitterIconContainer: `text-3xl m-4`,
+  tweetButton: `bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold rounded-3xl h-[50px] mt-[20px] cursor-pointer`,
+  navContainer: `flex-1`,
+  profileButton: `flex items-center mb-6 cursor-pointer hover:bg-[#333c45] rounded-[100px] p-2`,
+  profileLeft: `flex item-center justify-center mr-4`,
+  profileImage: `height-12 w-12 rounded-full`,
+  profileRight: `flex-1 flex`,
+  details: `flex-1`,
+  name: `text-lg`,
+  handle: `text-[#8899a6]`,
+  moreContainer: `flex items-center mr-2`,
+}
+
+
+function Sidebar() {
+    return (
+        <div className={style.wrapper}>
+            <div className={style.twitterIconContainer}>
+                <VscTwitter/>
+            </div>
+            <div>
+                Home
+            </div>
+            <div>
+                Explore
+            </div>
+            <div>
+                Notifications
+            </div>
+            <div>
+                Messages
+            </div>
+            <div>
+                Lists
+            </div>colo
+            <div>
+                Profile
+            </div>
+            <div>
+                More
+            </div>
+            <div className={style.tweetButton}>
+                Mint
+            </div>
+
+            <div className={style.profileButton}>
+                <div className={style.profileLeft}></div>
+                <div className={style.profileRight}>
+                    <div className={style.details}>
+                        <div className={style.name}>pruthvi_raj49</div>
+                        <div className={style.handle}>@0x22dh...4hsoal</div>
+                    </div>
+                </div>
+                <div className={style.moreContainer}>
+                    <FiMoreHorizontal/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '9ft3pfdu',
+    dataset: 'production'
+  }
+})
